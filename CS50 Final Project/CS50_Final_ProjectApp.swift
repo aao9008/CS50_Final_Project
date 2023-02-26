cf83b946d32412a1fb3a944aca42037cb59a2a87
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-02-26 00:15:15 -0500

    Initial Commit

---
+++ CS50 Final Project/CS50_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS50_Final_ProjectApp.swift
+//  CS50 Final Project
+//
+//  Created by Alfredo Ormeno Zuniga on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct CS50_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CS50 Final Project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CS50 Final Project
+//
+//  Created by Alfredo Ormeno Zuniga on 2/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
